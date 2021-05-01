2a68ff86661f074fa72e2175cfc10badb6ec7e64
Vincenzo <vincebshock@gmail.com>
2021-05-01 06:54:15 +0200

    ✨Component: pomodoro-timer added

---
+++ cromato-client-reactjs/src/components/pomodoro-timer/PomodoroTimer.js
@@ -0,0 +1,15 @@
+import { Component } from 'react';
+import './PomodoroTimer.css';
+
+class PomodoroTimer extends Component {
+  state = {};
+  render() {
+    return (
+      <div className="pomodoro-timer">
+        <div className="wrapper">pomodoroTimer</div>
+      </div>
+    );
+  }
+}
+
+export default PomodoroTimer;
