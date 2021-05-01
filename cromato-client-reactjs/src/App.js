1bc8fe283d6d0c1d6c8dee6a1efebd036a5ff08d
Vincenzo <vincebshock@gmail.com>
2021-05-01 05:35:19 +0200

    ✨Basic routing added

--- cromato-client-reactjs/src/App.js
+++ cromato-client-reactjs/src/App.js
@@ -1,7 +1,17 @@
+import { Route, BrowserRouter, Switch } from 'react-router-dom';
 import './App.css';
+import TimerPage from './pages/timer/TimerPage';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="cromato">
+      <BrowserRouter>
+        <Switch>
+          <Route exact={true} path="/" component={TimerPage} />
+        </Switch>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
