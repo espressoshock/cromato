3a035c387fce2d28e47e08f55887816497df63fa
Vincenzo <vincebshock@gmail.com>
2021-05-25 19:28:12 +0200

    serviceKey.json -> .env

--- index.js
+++ index.js
@@ -9,7 +9,18 @@ require('./passport-setup');
 
 //firebase admin sdk
 const admin = require('firebase-admin');
-const serviceAccount = require('./serviceAccountKey.json');
+const serviceAccount = {
+  type: process.env.FASK_TYPE,
+  project_id: process.env.FASK_PROJECT_ID,
+  private_key_id: process.env.FASK_PRIVATE_KEY_ID,
+  private_key: process.env.FASK_PRIVATE_KEY,
+  client_email: process.env.FASK_CLIENT_MAIL,
+  client_id: process.env.FASK_CLIENT_ID,
+  auth_uri: process.env.FASK_AUTH_URI,
+  token_uri: process.env.FASK_TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.FASK_CERT_URL_PROVIDER,
+  client_x509_cert_url: process.env.FASK_CERT_URL_CLIENT,
+};
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
