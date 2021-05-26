9e24df33a9c383b2c9909203aa8a88dd1645fddd
Vincenzo <vincebshock@gmail.com>
2021-05-26 10:22:38 +0200

    🚀call-back relative path update

--- passport-setup.js
+++ passport-setup.js
@@ -15,7 +15,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: 'http://localhost:5000/api/auth/callback',
+      callbackURL: '/api/auth/callback',
     },
     async (accessToken, refreshToken, profile, done) => {
       // console.log('profile: ', profile);
